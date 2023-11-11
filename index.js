9156890a4e933d163da37c4da69247f5f96aa669
SakibLWD <sagorahmed6480@gmail.com>
2023-11-11 15:03:28 +0600

    update

--- index.js
+++ index.js
@@ -5,24 +5,24 @@ function numFunction(value){
 }
 
 function addFunction(operator){
-    let inputField = document.querySelector(".input");
-    inputField.value += operator;
+    let inputOpr = document.querySelector(".input");
+    inputOpr.value += operator;
 }
 
 function subFunction(operator){
-    let inputField = document.querySelector(".input");
-    inputField.value += operator;
+    let inputOpr = document.querySelector(".input");
+    inputOpr.value += operator;
 
 }
 
 function multFunction(operator){
-    let inputField = document.querySelector(".input");
-    inputField.value += operator;
+    let inputOpr = document.querySelector(".input");
+    inputOpr.value += operator;
 }
 
 function divFunction(operator){
-    let inputField = document.querySelector(".input");
-    inputField.value += operator;
+    let inputOpr = document.querySelector(".input");
+    inputOpr.value += operator;
 }
 
 function delFunction(){
@@ -32,7 +32,41 @@ function delFunction(){
 function calFunction(){
     let inputField = document.querySelector(".input");
     let expression = inputField.value;
-    inputField.value = eval(expression);
+    // inputField.value = eval(expression);
+
+let operands = expression.split(/[-+*/]/);
+let operator = expression.match(/[^\d]/);
+
+if(operands.length === 2 && operator){
+    let operand1 = parseFloat(operands[0]);
+    let operand2 = parseFloat(operands[1]);
+
+    switch(operator[0]) {
+        case "+":
+            inputField.value = operand1 + operand2;
+            break;
+        
+        case "-":
+            inputField.value = operand1 - operand2;
+            break;
+            
+        case "*":
+            inputField.value = operand1 - operand2;
+            break;
+
+        case "/":
+            if(operand2 != 0){
+                inputField.value = operand1 / operand2;
+            }else{
+                inputField.value = "Error"
+            }
+            break;
+        default:
+            break;
+
+    }
+}
+
 
 }
     
