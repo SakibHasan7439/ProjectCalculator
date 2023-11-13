df46fc45b9ba9b62f30e78c51fc5924c49c44301
SakibLWD <sagorahmed6480@gmail.com>
2023-11-13 14:24:11 +0600

    update

--- index.js
+++ index.js
@@ -1,37 +1,65 @@
 
+//Empty variables
+let currentValue = "";
+let firstValue = "";
+let operator = "";
+
+//function to create the values and the current value
 function numFunction(value){
     let inputField =  document.querySelector(".input");
     inputField.value += value
+    currentValue = inputField.value;
 }
 
-function addFunction(operator){
-    let inputOpr = document.querySelector(".input");
-    inputOpr.value += operator;
+//function to convert the current value into first value and set the operator
+function storeFirstValue(opr){
+    if(currentValue !== ""){
+        operator = opr;
+        firstValue = currentValue;
+        console.log(firstValue)
+        //problem
+        currentValue = "";
+        updateDisplay()
+    }
+
 }
 
-function subFunction(operator){
-    let inputOpr = document.querySelector(".input");
-    inputOpr.value += operator;
-
-}
-
-function multFunction(operator){
-    let inputOpr = document.querySelector(".input");
-    inputOpr.value += operator;
-}
-
-function divFunction(operator){
-    let inputOpr = document.querySelector(".input");
-    inputOpr.value += operator;
-}
-
+//function to clear the input field 
 function delFunction(){
     document.getElementById("demo").value = "";
 }
 
+//function to calculate the values
 function calFunction(){
-    let inputField = document.querySelector(".input");
-    let expression = inputField.value;
-    inputField.value = eval(expression);
+    let result;
+    switch(operator){
+        case "+":
+            result = parseFloat(firstValue) + parseFloat(currentValue);
+            break;
+
+        case "-":
+            result = parseFloat(firstValue) - parseFloat(currentValue);
+            break;
+
+        case "*":
+            result = parseFloat(firstValue) * parseFloat(currentValue);
+            break;
+
+        case "/":
+            if(firstValue == 0){
+                result = "Zero divison error";
+            }else{
+                result = parseFloat(firstValue) / parseFloat(currentValue);
+            }
+
+            break;
+        default:
+            break;
+    }
+    document.querySelector(".input").value = Math.round(result * 100)/100;
+    operator = "";
+    firstValue = "";
 }
-    
+function updateDisplay(){
+    document.querySelector(".input").value = currentValue;
+}
